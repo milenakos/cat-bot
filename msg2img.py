6558ba1bb41cc5cbb446c3a709ead1aaae1f3f6a
Milenakos <75163549+milena-kos@users.noreply.github.com>
2023-01-17 15:09:05 +0000

    fix bot spacing

--- msg2img.py
+++ msg2img.py
@@ -120,9 +120,9 @@ def msg2img(message, bot, text_override=None):
 
         pencil.rounded_rectangle(
             (
-                129 + font.getsize(nick)[0] + 34 + 5,
+                129 + font.getsize(nick)[0] + 9,
                 8 + 6,
-                129 + font.getsize(nick)[0] + 48 + botfont.getsize("BOT")[0],
+                129 + font.getsize(nick)[0] + 18 + botfont.getsize("BOT")[0],
                 10 + 7 + 25,
             ),
             fill=(88, 101, 242),
@@ -130,12 +130,12 @@ def msg2img(message, bot, text_override=None):
         )
 
         pencil.text(
-            (131 + font.getsize(nick)[0] + 42, 10 + 6),
+            (131 + font.getsize(nick)[0] + 12, 10 + 6),
             "BOT",
             font=botfont,
             fill=(255, 255, 255),
         )
-        move = botfont.getsize("BOT")[0] + 55
+        move = botfont.getsize("BOT")[0] + 15
     pencil.text(
         (122, 55), text.strip(), font=font2, fill=(255, 255, 255)
     )  # draw message text
