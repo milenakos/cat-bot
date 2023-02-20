147f61b7be98cc872ae6e74488414bfc91f7fe43
Milenakos <75163549+milena-kos@users.noreply.github.com>
2023-02-20 17:18:17 +0000

    global

--- main.py
+++ main.py
@@ -240,6 +240,7 @@ async def myLoop():
 
 @bot.event
 async def on_ready():
+    global milenakoos
     print("cat is now online")
     await bot.change_presence(
             activity=discord.Activity(type=discord.ActivityType.playing, name=f"/help | Providing life support for {len(bot.guilds)} servers")
