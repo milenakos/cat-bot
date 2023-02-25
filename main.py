895f46f01ed327cce9e9d7793a0f2013a6b1ac33
Milenakos <75163549+milena-kos@users.noreply.github.com>
2023-02-25 14:18:11 +0000

    fix

--- main.py
+++ main.py
@@ -964,12 +964,12 @@ async def trade(message: discord.Interaction, person_id: discord.Member):
             
             await interaction.response.defer()
             await interaction.message.edit("Trade finished!", embed=None, view=None)
-            if not has_ach(message.guild.id, person1, "introvert"):
-                ach_data = give_ach(message.guild.id, person1, "introvert")
+            if not has_ach(message.guild.id, person1, "extrovert"):
+                ach_data = give_ach(message.guild.id, person1, "extrovert")
                 embed = discord.Embed(title=ach_data["title"], description=ach_data["description"], color=0x007F0E).set_author(name="Achievement get!", icon_url="https://pomf2.lain.la/f/hbxyiv9l.png")
                 await message.channel.send(embed=embed)
-            if not has_ach(message.guild.id, person2, "introvert"):
-                ach_data = give_ach(message.guild.id, person2, "introvert")
+            if not has_ach(message.guild.id, person2, "extrovert"):
+                ach_data = give_ach(message.guild.id, person2, "extrovert")
                 embed = discord.Embed(title=ach_data["title"], description=ach_data["description"], color=0x007F0E).set_author(name="Achievement get!", icon_url="https://pomf2.lain.la/f/hbxyiv9l.png")
                 await message.channel.send(embed=embed)
             return
