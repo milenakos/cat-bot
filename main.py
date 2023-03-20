0f1e1a9cdcd89fbdd1cfb62ac9919d347189c6d5
Milenakos <75163549+milena-kos@users.noreply.github.com>
2023-03-20 10:25:43 +0000

    ugh 2

--- main.py
+++ main.py
@@ -572,9 +572,10 @@ async def on_message(message):
 @bot.slash_command(description="no way its cat rain")
 async def rain(message: discord.Interaction, type: str = discord.SlashOption(choices=["shortrain", "normalrain", "longrain"])):
     if get_cat(type, message.user.id, type) != 0:
-        typemap = {"short": 5, "normal": 25, "long": 50}
+        typemap = {"shortrain": 5, "normalrain": 25, "longrain": 50}
         amount = typemap[type]
         add_cat(message.guild.id, message.user.id, "Fine", amount)
+        remove_cat(type, message.user.id, type, 1)
         await message.response.send_message(f":umbrella: Cats rained and you got {amount} Fine cats!")
     else:
         await message.response.send_message("You don't have cat rain of that type!", ephemeral=True)
