0e7463ec6490ef516a2c221d47cd23489c081676
Milenakos <business@milenakos.tk>
2023-07-13 18:19:50 +0000

    what is this supposed to indicate :staring_cat:

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ GITHUB_CHANNEL_ID = 1060965767044149249
 
 BANNED_ID = [1029044762340241509] # banned from using /tiktok
 
-WHITELISTED_BOTS = [1087001524774912050] # bots which are allowed to catch cats
+WHITELISTED_BOTS = [1087001524774912050, 823896940847824936] # bots which are allowed to catch cats
 
 ### Setup values end
 
