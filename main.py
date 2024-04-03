f489deff7ca29d41378c14fbceeea63a10d5db39
Milenakos <git@minkos.lol>
2024-04-03 14:59:06 +0000

    message cache thingy

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ ach_names = ach_list.keys()
 ach_titles = {value["title"].lower(): key for (key, value) in ach_list.items()}
 
 intents = discord.Intents(message_content=True, messages=True, reactions=True, guilds=True)
-bot = commands.AutoShardedBot(command_prefix="https://www.youtube.com/watch?v=dQw4w9WgXcQ", intents=intents, help_command=None, max_messages=None)
+bot = commands.AutoShardedBot(command_prefix="https://www.youtube.com/watch?v=dQw4w9WgXcQ", intents=intents, help_command=None)
 
 # this list stores unique non-duplicate cattypes
 cattypes = []
