872accba5b3343b77f1c1387aac86c8ef0bf64e9
Lia Milenakos <git@minkos.lol>
2024-05-09 17:28:17 +0000

    fix some other stuff

--- main.py
+++ main.py
@@ -2329,11 +2329,11 @@ async def leaderboards(message: discord.Interaction, leaderboard_type: Optional[
         messager_placement = 1
         if interactor:
             for i in the_dict.values():
-                if interactor < i:
+                if interactor <= i:
                     interactor_placement += 1
         if messager and message.user.id != interaction.user.id:
             for i in the_dict.values():
-                if messager < i:
+                if messager <= i:
                     messager_placement += 1
 
         # rarest cat display
