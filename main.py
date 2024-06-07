f597727e2af7ab256e08d375abb2585f866ac957
Lia Milenakos <git@minkos.lol>
2024-06-07 17:06:05 +0000

    comply with dev tos updates

--- main.py
+++ main.py
@@ -1496,7 +1496,7 @@ async def ping(message: discord.Interaction):
 
 @bot.tree.command(description="give cats now")
 @discord.app_commands.rename(cat_type="type")
-@discord.app_commands.describe(person="Whom to donate?", cat_type="Select a donate cat type", amount="And how much?")
+@discord.app_commands.describe(person="Whom to donate?", cat_type="Select a fucking cat type", amount="And how much?")
 @discord.app_commands.autocomplete(cat_type=cat_type_autocomplete)
 async def gift(message: discord.Interaction, person: discord.User, cat_type: str, amount: Optional[int]):
     if not amount: amount = 1  # default the amount to 1
