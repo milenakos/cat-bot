3b03d88fd0de44f7797704d8ddbb61eb8d49202a
VishramKidPG123 <94813389+VishramKidPG123@users.noreply.github.com>
2024-06-21 14:52:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1094,6 +1094,8 @@ async def catinfo(message: discord.Interaction, cat_type: str):
         await message.response.send_message("cat does not exist", ephemeral=True)
         return
     icon = get_emoji(cat_type.lower() + "cat")
+    s = sum(type_dict.values())
+    v = type_dict[cat_type]
     embed = discord.Embed(title=f"{icon} {cat_type} Cat Info", color=0x6E593C) # possibly make color different per cat type
     embed.add_field(name="LORE", value=lore[cat_type], inline=False)
     embed.add_field(name="You have:", value=get_cat(message.guild.id, message.user.id, cat_type), inline=True)
