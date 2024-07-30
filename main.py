937af2f868b15e62aa07ce0424514ae5f6c00b50
Lia Milenakos <git@minkos.lol>
2024-07-30 16:15:15 +0300

    remove /warning
    its outdated with new cat types

--- main.py
+++ main.py
@@ -2009,11 +2009,6 @@ async def cursed(message: discord.Interaction):
     file = discord.File("cursed.jpg", filename="cursed.jpg")
     await message.response.send_message(file=file)
 
-@bot.tree.command(description="Get a warning")
-async def warning(message: discord.Interaction):
-    file = discord.File("warning.png", filename="warning.png")
-    await message.response.send_message(file=file)
-
 @bot.tree.command(description="Get Your balance")
 async def bal(message: discord.Interaction):
     file = discord.File("money.png", filename="money.png")
