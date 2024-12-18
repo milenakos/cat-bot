8699e1107a941bb51f9400bd0237889b81cdb606
VishramKidPG123 <94813389+VishramKidPG123@users.noreply.github.com>
2024-12-18 13:43:16 -0500

    Move this because it was possible to like scam someone
    if you clicked add after having added cats and then the other person accepted while you had the trade menu open then you could remove your cats and then click accept to scam someone or something idk

--- main.py
+++ main.py
@@ -3173,10 +3173,7 @@ async def trade(message: discord.Interaction, person_id: discord.User):
             return
 
         currentuser = 1 if interaction.user == person1 else 2
-
-        person1accept = False
-        person2accept = False
-
+        
         # all we really do is spawn the modal
         modal = TradeModal(currentuser)
         await interaction.response.send_modal(modal)
@@ -3374,6 +3371,9 @@ async def trade(message: discord.Interaction, person_id: discord.User):
                 except Exception:
                     person2gives[self.cattype.value] = int(value)
 
+            person1accept = False
+            person2accept = False
+            
             await interaction.response.defer()
             await update_trade_embed(interaction)
 
