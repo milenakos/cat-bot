6c66a956edd93a94663e3c3590bd38e6dd01b06e
sillysnake3 <orion@justfour.us>
2025-01-06 15:38:59 -0500

    ok

--- main.py
+++ main.py
@@ -1990,7 +1990,8 @@ async def on_message(message: discord.Message):
                 image_binary.seek(0)
                 await bot.create_application_emoji(name=emoji_name, image=image_binary.getvalue())
 
-        user.custom_num = stuff[3]
+        try:
+            user.custom_num = stuff[3]
         except Exception:
             pass
 
@@ -2521,11 +2522,8 @@ async def gen_inventory(message, person_id):
             give_collector = False
 
     if user.custom:
-        custom_num = 1
-        if user.custom_num:
-            custom_num = user.custom_num
         icon = get_emoji(user.custom.lower().replace(" ", "") + "cat")
-        embedVar.add_field(name=f"{icon} {user.custom}", value=custom_num, inline=True)
+        embedVar.add_field(name=f"{icon} {user.custom}", value=user.custom_num, inline=True)
 
     if is_empty and not user.custom:
         embedVar.add_field(name="None", value=f"u hav no cats {get_emoji('cat_cry')}", inline=True)
