c9276c30e24340a1804d960a2b95cac1c071c243
Lia Milenakos <git@minkos.lol>
2025-01-22 19:22:20 +0300

    fix

--- main.py
+++ main.py
@@ -2532,9 +2532,17 @@ async def gen_inventory(message, person_id):
     else:
         color = "#6E593C"
 
+    try:
+        needed_xp = battle["seasons"][str(person.season)][person.battlepass]["xp"]
+    except Exception:
+        if person.battlepass != 0:
+            needed_xp = 1500
+        else:
+            needed_xp = 0
+
     embedVar = discord.Embed(
         title=f"{emoji_prefix}{person_id.name}",
-        description=f"⏱️ Fastest: {catch_time}s, Slowest: {slow_time}h\n{get_emoji('cat_throphy')} Achievements: {unlocked}/{total_achs}{minus_achs}\n⬆️ Battlepass Level {person.battlepass} ({person.progress}/{battle['seasons'][str(person.season)][person.battlepass]['xp']} XP)",
+        description=f"⏱️ Fastest: {catch_time}s, Slowest: {slow_time}h\n{get_emoji('cat_throphy')} Achievements: {unlocked}/{total_achs}{minus_achs}\n⬆️ Battlepass Level {person.battlepass} ({person.progress}/{needed_xp} XP)",
         color=discord.Colour.from_str(color),
     )
 
