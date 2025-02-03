b003bcfa7e868976ddc8fe669290a0a44ee4ee47
Lia Milenakos <git@minkos.lol>
2025-02-03 16:49:07 +0300

    remove ads and bold more things in /info

--- main.py
+++ main.py
@@ -2215,8 +2215,7 @@ async def info(message: discord.Interaction):
     embedVar = discord.Embed(
         title="Cat Bot",
         color=0x6E593C,
-        description="[Join support server](https://discord.gg/staring)\n[GitHub Page](https://github.com/milenakos/cat-bot)\n\n"
-        + f"by {gen_credits['author']}\nWith contributions from {gen_credits['contrib']}.\n\nThis bot adds Cat Hunt to your server with many different types of cats for people to discover! People can see leaderboards and give cats to each other.\n\n"
+        description=f"by **{gen_credits['author']}**\nWith contributions from *{gen_credits['contrib']}*.\n\nThis bot adds Cat Hunt to your server with many different types of cats for people to discover! People can see leaderboards and give cats to each other.\n\n"
         + f"Thanks to:\n**pathologicals** for the cat image\n**thecatapi.com** for random cats API\n**catfact.ninja** for cat facts API\n**countik** for TikTok TTS API\n**{gen_credits['trash']}** for making cat, suggestions, and a lot more.\n\n**{gen_credits['tester']}** for being test monkeys\n\n**And everyone for the support!**",
     ).set_thumbnail(url="https://wsrv.nl/?url=raw.githubusercontent.com/milenakos/cat-bot/main/images/cat.png")
 
