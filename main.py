df9b481475983f6e8e288663d4a9b51d679b7a79
Lia Milenakos <git@minkos.lol>
2025-03-05 22:06:14 +0300

    fix msg2img
    i actually didnt fix anything but it works now so im bringing it back

--- main.py
+++ main.py
@@ -23,11 +23,11 @@ from aiohttp import web
 from discord import ButtonStyle
 from discord.ext import commands
 from discord.ui import Button, View
-# from PIL import Image
+from PIL import Image
 
 import config
 
-# import msg2img
+import msg2img
 from database import Channel, Prism, Profile, Reminder, User, db
 
 if config.COLLECT_STATS:
@@ -2209,15 +2209,13 @@ async def on_message(message: discord.Message):
 
             if og_msg.attachments[0].content_type.startswith("image/gif"):
                 await bot.create_application_emoji(name=emoji_name, image=img_data)
-            """
-            elif False:
+            else:
                 img = Image.open(io.BytesIO(img_data))
                 img.thumbnail((128, 128))
                 with io.BytesIO() as image_binary:
                     img.save(image_binary, format="PNG")
                     image_binary.seek(0)
                     await bot.create_application_emoji(name=emoji_name, image=image_binary.getvalue())
-"""
         user.custom = cat_name if cat_name != "None" else ""
         emojis = {emoji.name: str(emoji) for emoji in await bot.fetch_application_emojis()}
         user.save()
@@ -5761,13 +5759,10 @@ async def catch(message: discord.Interaction, msg: discord.Message):
         return
     await message.response.defer()
 
-    """
     event_loop = asyncio.get_event_loop()
     result = await event_loop.run_in_executor(None, msg2img.msg2img, msg)
 
     await message.followup.send("cought in 4k", file=result)
-    """
-    await message.followup.send("cought in 4k (this is broken rn sry)")
 
     catchcooldown[message.user.id] = time.time()
 
