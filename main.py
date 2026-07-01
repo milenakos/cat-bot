9e8b0f86074e9294d1f6e46bc5a343a57e7eea48
Lia Milenakos <lia@minkos.lol>
2026-07-01 15:45:58 +0300

    add fallback for missing cattype (???)

--- main.py
+++ main.py
@@ -2328,6 +2328,8 @@ async def on_message(message: discord.Message):
         channel = await Channel.get_or_none(channel_id=message.channel.id)
         if not server:
             server = await Server.get_or_create(server_id=message.guild.id)
+        if not channel.cattype:
+            return
         if (
             not channel
             or not channel.cat
