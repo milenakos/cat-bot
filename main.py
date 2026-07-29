e0fa15b7cf7ce22ba0f63cb2237a457b20bf135c
Lia Milenakos <lia@minkos.lol>
2026-07-29 05:21:22 +0300

    fix others being able to press main

--- main.py
+++ main.py
@@ -6348,6 +6348,8 @@ async def fish(message: discord.Interaction):
             fish_lock.discard((interaction.guild.id, interaction.user.id))
 
     async def show_main(interaction: discord.Interaction) -> None:
+        if interaction.user != message.user:
+            return await do_funny(interaction)
         await interaction.response.defer()
         await interaction.edit_original_response(view=main_view())
 
