8ede38c60bfd693df11380d2bd1c30deb76d212b
Lia Milenakos <lia@minkos.lol>
2026-09-11 01:41:25 +0300

    /packs: use 4 buttons per row

--- main.py
+++ main.py
@@ -5701,8 +5701,8 @@ async def packs(message: discord.Interaction):
             buttons.append(button)
 
         rows = []
-        for i in range(0, len(buttons), 5):
-            rows.append(ActionRow(*buttons[i : i + 5]))
+        for i in range(0, len(buttons), 4):
+            rows.append(ActionRow(*buttons[i : i + 4]))
 
         return rows, has_special, total_packs
 
